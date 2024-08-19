b569c4ca5cbd3da53be1ad5194cf67d96c033e30
Orxan Huseynov <115917125+Orxan044@users.noreply.github.com>
2024-08-19 11:50:05 +0400

    Add project files.

---
+++ TaskManeger-WPF/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows;
+
+namespace TaskManeger_WPF;
+
+public partial class MainWindow : Window
+{
+
+
+    ObservableCollection<Process> processesList = new(Process.GetProcesses());
+    
+    
+    ProcessStartInfo process = new();
+    public MainWindow()
+    {
+        InitializeComponent();
+        ListView1.ItemsSource = processesList;
+    }
+
+    private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        string text = txtBox.Text;
+        
+        try
+        {
+            if(text is not null)
+            {
+                process.FileName = text;
+                var p = Process.Start(process);
+                processesList.Add(p!);
+
+            }
+        }
+        catch (Exception  ex)
+        {
+            MessageBox.Show(ex.ToString());
+        }
+        
+    }
+    
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+
+        if(ListView1.SelectedItem is not null)
+        {
+            var select = ListView1.SelectedItem as Process;
+            //ListView1.Items.Remove(select);
+            processesList.Remove(select!);
+
+            select!.Kill();
+
+        }
+    }
+
+    private void ListView1_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+
+    }
+}
